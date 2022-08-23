77e82b0a665f213b1f5882404558dd537581c6dc
kaslufl <leallucasfonseca@gmail.com>
2022-08-23 20:50:27 -0300

    feat(EC-11): add Product entity

---
+++ src/main/java/com/example/ecommerce/model/entity/Product.java
@@ -0,0 +1,40 @@
+package com.example.ecommerce.model.entity;
+
+public class Product {
+    private Integer id;
+    private String name;
+    private Float price;
+    private String photoUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+}
